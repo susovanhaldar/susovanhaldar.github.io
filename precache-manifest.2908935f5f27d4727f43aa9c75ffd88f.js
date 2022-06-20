48089868b538a342ae0868e19983c8e2a0fd1c46
susovan <susovanimp@gmail.com>
2022-06-20 22:49:06 +0530

    Updates

---
+++ precache-manifest.2908935f5f27d4727f43aa9c75ffd88f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d87eeb9696269374966426f094f2ed",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e04046b89219f0b61496",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "f73469c63ee0809c77a7",
+    "url": "/home/static/js/2.2c19a9a7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.2c19a9a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04046b89219f0b61496",
+    "url": "/home/static/js/main.7753da0e.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "6ae8d785644d780847e7314005cac1e7",
+    "url": "/home/static/media/susovanhaldar.6ae8d785.JPG"
+  }
+]);
